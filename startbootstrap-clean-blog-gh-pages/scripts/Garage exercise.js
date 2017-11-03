6ce16f35d595b940e8bfbd53e8d4f4527ae15f6c
ConcettoDimauro <c.dimauro94@hotmail.com>
2017-11-03 12:36:01 +0000

    commit

---
+++ startbootstrap-clean-blog-gh-pages/scripts/Garage exercise.js
@@ -0,0 +1,81 @@
+function car(reg, make, year, faults, checkedIn) {
+    return {
+
+        reg: reg,
+        make: make,
+        year: year,
+        faults: faults,
+        checkedIn: checkedIn,
+    }
+}
+let carlist = [];
+
+carlist.push(car("NA54 KTE", "vw", 2003, 2, true))
+carlist.push(car("SR31 QWW", "bmw", 2000, 6, true))
+carlist.push(car("NW19 HZJ", "kia", 2003, 0, true))
+carlist.push(car("SR4I OQP", "vw", 2006, 4, true))
+carlist.push(car("TY78 IJK", "bmw", 2002, 1, true))
+carlist.push(car("FG29 IJL", "kia", 2009, 3, true))
+
+function createNewcar() {
+    let reg = document.getElementById('reg').value
+    let make = document.getElementById('make').value
+    let year = document.getElementById('year').value
+    let faults = document.getElementById('faults').value
+    var isCheckedin = false;
+    if (document.getElementById('checkedIn').checked) {
+        isChecked = true;
+
+    } else {
+        isChecked = false;
+
+
+    }
+
+
+    let newcar = car(reg, make, year, faults, isChecked)
+    console.log(newcar);
+    alert("you've check in a new car!")
+}
+
+function getGarageContents() {
+    let searchQuery = document.getElementById("searchQuery").value;
+    let para = document.getElementById("results");
+    para.innerHTML = "";
+    if (searchQuery == "") {
+        para.innerHTML = "enter something dairylea dipshit";
+
+    }
+    else {
+
+
+        for (let i = 0; i < carlist.length; i++) {
+            //console.log(carlist[i][key].toString().toUpperCase());
+            if (carlist[i].reg.toUpperCase().includes(searchQuery.toUpperCase())) {
+                console.log(carlist[i].reg);
+                console.log(carlist[i].make);
+                console.log(carlist[i].year);
+                console.log(carlist[i].faults);
+                console.log(carlist[i].checkedIn);
+                para.innerHTML += carlist[i].reg + carlist[i].make + carlist[i].year + carlist[i].faults + carlist[i].checkedIn + "<br>";
+
+            }
+
+        }
+
+    }
+
+}
+
+function checkoutCar() {
+
+    for (let i = 0; i < carlist.length; i++) {
+        if (carlist[i].reg.toUpperCase().includes(searchQuery.toUpperCase())) {
+            carlist[i].checkedIn = false
+        }
+
+
+
+
+    }
+}
